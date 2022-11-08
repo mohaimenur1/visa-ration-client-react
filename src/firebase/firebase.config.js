b5a5b282413ebdf1218abd3877ba1ef87c9495cc
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-08 19:57:37 +0600

    initialize firebase into app

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcFAucC0AT3U3NYpAbAg6j7x-mvMGwblA",
+  authDomain: "visaration-cd5b6.firebaseapp.com",
+  projectId: "visaration-cd5b6",
+  storageBucket: "visaration-cd5b6.appspot.com",
+  messagingSenderId: "56572973568",
+  appId: "1:56572973568:web:b0ec448766f705dcb43ee5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
