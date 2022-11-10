00814a8d99a8e3e3c7d78b9b14c2c9ce3c6686c3
Mohai Menur <mmrahmanrahi@gmail.com>
2022-11-10 17:50:27 +0600

    spinner implemented

--- src/Page/ServicePage/ServicePage.js
+++ src/Page/ServicePage/ServicePage.js
@@ -1,13 +1,27 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import useTitle from '../../hooks/useTitle';
 
 const ServicePage = () => {
   const services = useLoaderData();
+  const [loading, setLoading] = useState(true);
   useTitle('Services');
+  if (loading) {
+    return (
+      <div className='text-center mt-4'>
+        <div
+          className='spinner-grow text-danger'
+          style={{ width: '3rem', height: '3rem' }}
+          role='status'
+        >
+          <span className='visually-hidden'>Loading...</span>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className='container'>
       <h2>ServicePage</h2>
