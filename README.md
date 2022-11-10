58fc70c3b95743066c6e369cddb436a7d401fb5b
Mohai Menur <mmrahmanrahi@gmail.com>
2022-11-10 15:47:14 +0600

    readme modified

--- README.md
+++ README.md
@@ -1,70 +1,40 @@
-# Getting Started with Create React App
+<!-- @format -->
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Visaration
 
-## Available Scripts
+Live website link [Visaration].
 
-In the project directory, you can run:
+## Project Description
 
-### `npm start`
+---
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Description of project:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- In Visaration project user can taking services according their needs.
+- User can Register with email and Password.Google SignIn Facility also avaiable.
+- User can add reviews to services.
+- User can delete and also edit their reviews After login.
+- User can also add services.
+- Title of the page Change with the page a user can visit.
 
-### `npm test`
+## Technologies used in Project
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+---
 
-### `npm run build`
+Technologies:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- Front-End :-
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+1. Css FrameWork = Bootstrap
+2. Js Library = React Js
+3. Authentication System = Firebase Auth
+4. Client Side Routing = React Router Dom
+5. picture will be viewed on full screen = react-photo-view Npm package
+6. Icons = Fontawsome
+7. Hosting = Firebase Hosting
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+- Back-End :-
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+1. RESTful APIs = Express Js
+2. Allow Access with Middleware = Cors Node.js package
+3. Backend Hosting = Vercel
--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -1,57 +1,57 @@
 /** @format */
 
-import React from "react";
-import { Link } from "react-router-dom";
-import Air from "./img/air.png";
-import File from "./img/files.png";
-import viva from "./img/viva.png";
-import { useLoaderData } from "react-router-dom";
-import ServiceCard from "../../components/ServiceCard/ServiceCard";
-import HomeCard from "../../components/HomeCard/HomeCard";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Air from './img/air.png';
+import File from './img/files.png';
+import viva from './img/viva.png';
+import { useLoaderData } from 'react-router-dom';
+import ServiceCard from '../../components/ServiceCard/ServiceCard';
+import HomeCard from '../../components/HomeCard/HomeCard';
 
 const HomePage = () => {
   const services = useLoaderData();
   return (
     <div>
-      <div className="banner container">
+      <div className='banner container'>
         <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="false"
+          id='carouselExampleCaptions'
+          className='carousel slide'
+          data-bs-ride='false'
         >
-          <div className="carousel-indicators">
+          <div className='carousel-indicators'>
             <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
+              type='button'
+              data-bs-target='#carouselExampleCaptions'
+              data-bs-slide-to='0'
+              className='active'
+              aria-current='true'
+              aria-label='Slide 1'
             ></button>
             <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
+              type='button'
+              data-bs-target='#carouselExampleCaptions'
+              data-bs-slide-to='1'
+              aria-label='Slide 2'
             ></button>
             <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
+              type='button'
+              data-bs-target='#carouselExampleCaptions'
+              data-bs-slide-to='2'
+              aria-label='Slide 3'
             ></button>
           </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={Air} className="d-block h-75 img-fluid" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
+          <div className='carousel-inner'>
+            <div className='carousel-item active'>
+              <img src={Air} className='d-block h-75 img-fluid' alt='...' />
+              <div className='carousel-caption d-none d-md-block'>
                 <h1>Flight Booking?</h1>
                 <p>I am here to collect Flight ticket with best price.</p>
               </div>
             </div>
-            <div className="carousel-item">
-              <img src={File} className="d-block h-75 img-fluid" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
+            <div className='carousel-item'>
+              <img src={File} className='d-block h-75 img-fluid' alt='...' />
+              <div className='carousel-caption d-none d-md-block'>
                 <h1>Unable to prepare Files?</h1>
                 <p>
                   No worries Files are organised correctly and maintain with
@@ -59,9 +59,9 @@ const HomePage = () => {
                 </p>
               </div>
             </div>
-            <div className="carousel-item">
-              <img src={viva} className="d-block h-75 img-fluid" alt="..." />
-              <div className="carousel-caption d-none d-md-block">
+            <div className='carousel-item'>
+              <img src={viva} className='d-block h-75 img-fluid' alt='...' />
+              <div className='carousel-caption d-none d-md-block'>
                 <h1>Scared about facing embassy</h1>
                 <p>
                   I will give you some tips to get ride of all your fears and
@@ -71,76 +71,76 @@ const HomePage = () => {
             </div>
           </div>
           <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
+            className='carousel-control-prev'
+            type='button'
+            data-bs-target='#carouselExampleCaptions'
+            data-bs-slide='prev'
           >
             <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
+              className='carousel-control-prev-icon'
+              aria-hidden='true'
             ></span>
-            <span className="visually-hidden">Previous</span>
+            <span className='visually-hidden'>Previous</span>
           </button>
           <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
+            className='carousel-control-next'
+            type='button'
+            data-bs-target='#carouselExampleCaptions'
+            data-bs-slide='next'
           >
             <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
+              className='carousel-control-next-icon'
+              aria-hidden='true'
             ></span>
-            <span className="visually-hidden">Next</span>
+            <span className='visually-hidden'>Next</span>
           </button>
         </div>
       </div>
-      <div className="two-sections container mt-5">
-        <div className="row">
-          <div className="list-group col-lg-6">
+      <div className='two-sections container mt-5'>
+        <div className='row'>
+          <div className='list-group col-lg-6'>
             <button
-              type="button"
-              className="list-group-item list-group-item-action active"
-              aria-current="true"
+              type='button'
+              className='list-group-item list-group-item-action active'
+              aria-current='true'
             >
               File Ready?
             </button>
             <button
-              type="button"
-              className="list-group-item list-group-item-action"
+              type='button'
+              className='list-group-item list-group-item-action'
             >
               Passport Varified
             </button>
             <button
-              type="button"
-              className="list-group-item list-group-item-action"
+              type='button'
+              className='list-group-item list-group-item-action'
             >
               embassy Facing
             </button>
             <button
-              type="button"
-              className="list-group-item list-group-item-action"
+              type='button'
+              className='list-group-item list-group-item-action'
             >
               Police Varification
             </button>
             <button
-              type="button"
-              className="list-group-item list-group-item-action"
+              type='button'
+              className='list-group-item list-group-item-action'
               disabled
             >
               Flight Booking
             </button>
             <button
-              type="button"
-              className="list-group-item list-group-item-action"
+              type='button'
+              className='list-group-item list-group-item-action'
               disabled
             >
               Expert
             </button>
           </div>
-          <div className="col-lg-6">
-            <div className="h-100 p-5 bg-light border rounded-3">
+          <div className='col-lg-6'>
+            <div className='h-100 p-5 bg-light border rounded-3'>
               <h1>Get a better Service</h1>
               <p>
                 Providing a good quality of support . And trying to improve for
@@ -148,9 +148,9 @@ const HomePage = () => {
                 Services.
               </p>
               <Link
-                to="/service"
-                className="btn btn-outline-secondary"
-                type="button"
+                to='/service'
+                className='btn btn-outline-secondary'
+                type='button'
               >
                 Go to Services
               </Link>
@@ -159,12 +159,17 @@ const HomePage = () => {
         </div>
       </div>
       {/* service card */}
-      <div className="container mt-3">
-        <div className="row g-4">
+      <div className='container mt-3'>
+        <div className='row g-4'>
           {services.map((service) => {
             return <HomeCard key={service._id} service={service} />;
           })}
         </div>
+      </div>
+      <div className='text-center mt-3 mb-3'>
+        <Link to='/service' className='btn btn-primary'>
+          See All
+        </Link>
       </div>
     </div>
   );
