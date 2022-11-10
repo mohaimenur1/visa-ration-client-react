cc66f1b8fe3612c8cda063338c6d9e4eab5a92fd
Mohai Menur <mmrahmanrahi@gmail.com>
2022-11-10 06:59:04 +0600

    privet review route done

--- src/Page/MyReview/MyReview.js
+++ src/Page/MyReview/MyReview.js
@@ -1,13 +1,38 @@
 /** @format */
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/UserContext';
+import ReviewRow from './ReviewRow/ReviewRow';
 
 const MyReview = () => {
   const { user } = useContext(AuthContext);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, [user?.email]);
   return (
     <div>
-      <h2>my review page</h2>
+      <h2 className='text-center mt-3'>You have {reviews.length} reviews</h2>
+      <div className='review-table container mt-5'>
+        <table className='table align-middle mb-0 bg-white'>
+          <thead className='bg-light'>
+            <tr>
+              <th>Services</th>
+              <th>User</th>
+              <th>Review</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reviews.map((review) => {
+              return <ReviewRow key={review._id} review={review} />;
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/Page/MyReview/ReviewRow/ReviewRow.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import React, { useState, useEffect } from 'react';
+
+const ReviewRow = ({ review }) => {
+  const { service, servicename, textarea, useremail, username } = review;
+  const [reviewService, setReviewService] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/${service}`)
+      .then((res) => res.json())
+      .then((data) => setReviewService(data));
+  }, [service]);
+  return (
+    <>
+      <tr>
+        <td>
+          <div className='d-flex align-items-center'>
+            <img
+              src={reviewService.img}
+              alt=''
+              style={{ width: '45px', height: '45px' }}
+              className='rounded-circle'
+            />
+            <div className='ms-3'>
+              <p className='fw-bold mb-1'>{servicename}</p>
+              {/* <p className='text-muted mb-0'>john.doe@gmail.com</p> */}
+            </div>
+          </div>
+        </td>
+        <td>
+          <p className='fw-normal mb-1'>{username}</p>
+          <p className='text-muted mb-0'>{useremail}</p>
+        </td>
+
+        <td>{textarea}</td>
+        <td>
+          <button type='button' className='btn btn-link btn-sm btn-rounded'>
+            <i class='fa fa-pencil-square-o'></i>
+          </button>
+          <button type='button' className='btn btn-link btn-sm btn-rounded'>
+            <i class='fa fa-trash'></i>
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default ReviewRow;
