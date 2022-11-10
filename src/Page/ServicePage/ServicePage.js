1f62696a44d1db3efcdeb0826a8f51ea6dd8b88f
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-10 20:43:20 +0600

    adding loading

--- src/Page/ServicePage/ServicePage.js
+++ src/Page/ServicePage/ServicePage.js
@@ -1,36 +1,40 @@
 /** @format */
 
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import ServiceCard from '../../components/ServiceCard/ServiceCard';
-import useTitle from '../../hooks/useTitle';
+import React, { useState } from "react";
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import { AuthContext } from "../../context/UserContext";
+import useTitle from "../../hooks/useTitle";
 
 const ServicePage = () => {
   const services = useLoaderData();
-  const [loading, setLoading] = useState(true);
-  useTitle('Services');
-  if (loading) {
-    return (
-      <div className='text-center mt-4'>
-        <div
-          className='spinner-grow text-danger'
-          style={{ width: '3rem', height: '3rem' }}
-          role='status'
-        >
-          <span className='visually-hidden'>Loading...</span>
-        </div>
-      </div>
-    );
-  }
+  const { loading } = useContext(AuthContext);
+  useTitle("Services");
+
   return (
-    <div className='container'>
-      <h2>ServicePage</h2>
-      <div className='row g-3'>
-        {services.map((service) => {
-          return <ServiceCard key={service._id} service={service} />;
-        })}
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="text-center mt-4">
+          <div
+            className="spinner-grow text-danger"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="container">
+          <h2>ServicePage</h2>
+          <div className="row g-3">
+            {services.map((service) => {
+              return <ServiceCard key={service._id} service={service} />;
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
